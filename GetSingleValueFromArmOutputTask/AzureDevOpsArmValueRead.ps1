20d3184226b26438edd7ce196f63c944f2a5d2e8
Christian Waha <christian.waha@outlook.com>
2019-01-22 16:21:14 +0100

    Update Powershell

--- GetSingleValueFromArmOutputTask/AzureDevOpsArmValueRead.ps1
+++ GetSingleValueFromArmOutputTask/AzureDevOpsArmValueRead.ps1
@@ -7,11 +7,15 @@ Import-VstsLocStrings "$PSScriptRoot\Task.json"
 
 $armOutput = Get-VstsInput -Name armOutput
 $searchValue = Get-VstsInput -Name searchValue
-$readedValue = Get-VstsInput -Name readedValue
+$outputReadedValue = Get-VstsInput -Name outputReadedValue
+
+$armObjectFromJson = ConvertFrom-Json $armOutput
+$searchObject = Select-Object -InputObject $armObjectFromJson -Property $searchValue
+$value = $searchObject.Value 
 
 Write-Host $armOutput
 Write-Host $searchValue
-Write-Host $readedValue
+Write-Host $value
 
-
+Write-Host "##vso[task.setvariable variable=$outputReadedValue;]$value"
 
